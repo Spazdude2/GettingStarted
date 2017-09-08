4bc1b9324876ea14f5379010351fa4734997a19b
Spazdude2 <spaziani96@gmail.com>
2017-09-08 11:14:07 -0400

    Calculator

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an expression: ");
+            String equation = Console.ReadLine();
+            String[] parts = equation.Split(' ');
+
+            int num1 = Convert.ToInt32(parts[0]);
+            String operater = parts[1];
+            int num2 = Convert.ToInt32(parts[2]);
+            int answer = 0;
+
+
+            if (operater == "+")
+            {
+                answer = num1 + num2;
+            }
+
+            if (operater == "-")
+            {
+                answer = num1 - num2;
+            }
+
+            if (operater == "*")
+            {
+                answer = num1 * num2;
+            }
+
+            if (operater == "/")
+            {
+                answer = num1 / num2;
+            }
+
+            Console.WriteLine("Result: " + equation + " = " + answer);
+
+        }
+    }
+}
