88532395d54c27af21dbc29078426a8208c891c9
Spazdude2 <spaziani96@gmail.com>
2017-09-08 10:48:27 -0400

    Largest

---
+++ Largest/Largest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest
+{
+    class Largest
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input 1st number: ");
+            int response1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input 2nd number: ");
+            int response2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input 3rd number: ");
+            int response3 = Convert.ToInt32(Console.ReadLine());
+
+
+            if (response1 > response2 && response1 > response3)
+            {
+                Console.WriteLine("The 1st number is the largest of the three.");
+            }
+
+            if (response2 > response1 && response2 > response3)
+            {
+                Console.WriteLine("The 2nd number is the largest of the three.");
+            }
+
+            if (response3 > response1 && response3 > response2)
+            {
+                Console.WriteLine("The 3rd number is the largest of the three.");
+            }
+        }
+    }
+}
