8cf7c1a3ccac5c18fba23afc909c5bf61b3f4a27
Spazdude2 <spaziani96@gmail.com>
2017-09-08 23:10:48 -0400

    stuff for Moodle

---
+++ FavoriteFood/Favorite.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Favorite
+{
+    class Favorite
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your favorite food: ");
+            String response = Console.ReadLine();
+
+            //int num = Convert.ToInt32(Console.ReadLine());
+            if (response == "Pizza" || response == "pizza")
+            {
+                Console.WriteLine("We good bro");
+            }
+
+            else
+            {
+                Console.WriteLine("Get out of my face");
+            }
+        }
+    }
+}
