5e8b8bb1a68a985ed0fa11bb6a429dfb0dec2a6c
Spazdude2 <spaziani96@gmail.com>
2017-09-08 20:03:17 -0400

    Prime Solver
    Decided not to use for loops or arrays and just went with my trusty old
    if statements

---
+++ PrimeSolver/PrimeSolver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeSolver
+{
+    class PrimeSolver
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 2; x < 1000; x++)
+            {
+                int isPrime = 0;
+                for (int y = 1; y < x; y++)
+                {
+                    if (x % y == 0)
+                        isPrime++;
+
+                    if (isPrime == 2) break;
+                }
+                if (isPrime != 2)
+                    Console.Write(x + ", ");
+
+                isPrime = 0;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
